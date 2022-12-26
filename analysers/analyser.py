1394339eb49f3f3cae87dbbedba972f6243053b9
yoitz <yoitz@yandex.ru>
2022-12-26 13:39:12 +0300

    analyser-upd

--- analysers/analyser.py
+++ analysers/analyser.py
@@ -2,7 +2,15 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 from matplotlib.colors import ListedColormap, BoundaryNorm
 import numpy as np
+import sys
 
+import totex
+
+print("Args: ", sys.argv)
+print("Looking for file: ", sys.argv[1])
+
+NAME = ".".join(sys.argv[1].split("\\")[-1].split(".")[:-1])
+print("Output filenames: ", NAME)
 
 X = []
 Y = []
@@ -11,7 +19,8 @@ T = []
 KINETIC = []
 POTENTIAL = []
 ENERGY = []
-with open("./output/predcorr.txt", "r") as file:
+# "./output/predcorr.txt"
+with open(sys.argv[1], "r") as file:
     for line in file.readlines():
         # (
         #     ind,
@@ -43,30 +52,44 @@ with open("./output/predcorr.txt", "r") as file:
         ENERGY.append(KINETIC[-1] + POTENTIAL[-1])
         #points.append((x, y))
 
-fig, ax = plt.subplots(sharex=True, sharey=True)
-# plt.scatter(X, Y, c = np.tan(np.linspace(-1, 1, len(X))) ,marker=".")
-norm = plt.Normalize(0, max(T))
+if "-gp" in sys.argv:
+    fig, ax = plt.subplots(sharex=True, sharey=True)
+    # plt.scatter(X, Y, c = np.tan(np.linspace(-1, 1, len(X))) ,marker=".")
+    norm = plt.Normalize(0, max(T))
 
-points = np.array([X, Y]).T.reshape(-1, 1, 2)
-segments = np.concatenate([points[:-1], points[1:]], axis=1)
-lc = LineCollection(segments, cmap='viridis', norm=norm)
-# Set the values used for colormapping
-lc.set_array(np.array(T))
-lc.set_linewidth(1)
+    points = np.array([X, Y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    lc = LineCollection(segments, cmap='viridis', norm=norm)
+    # Set the values used for colormapping
+    lc.set_array(np.array(T))
+    lc.set_linewidth(0.1)
 
-line = ax.add_collection(lc)
-# fig.colorbar(line, ax=axs[0])
-ax.set_xlim(min(X)+0.2*min(X), 1.1*max(X))
-ax.set_ylim(min(Y)+0.2*min(X), 1.1*max(Y))
-fig.colorbar(line, ax=ax)
+    line = ax.add_collection(lc)
+    # fig.colorbar(line, ax=axs[0])
+    ax.set_xlim(min(X)+0.2*min(X), 1.1*max(X))
+    ax.set_ylim(min(Y)+0.2*min(X), 1.1*max(Y))
+    fig.colorbar(line, ax=ax)
+
+    #fig.savefig(f"./images/{NAME}-path.eps")
+
+    ax.set_ylim(Y[0]-1e2, max(Y)+1e2)
+    ax.set_xlim(X[0]-2*1e3, X[0]+10*1e3)
+    fig.savefig(f"./images/{NAME}-path-diforbit.eps")
 
 
-efig, eax = plt.subplots(sharex=True, sharey=True)
-eax.plot(T, ENERGY, label="full energy")
-eax.plot(T, KINETIC, label="kinetic energy")
-eax.plot(T, POTENTIAL, label="potential energy")
-eax.set_xlabel("t (s)")
-eax.set_ylabel("E/m J/kg")
+    efig, eax = plt.subplots(sharex=True, sharey=True)
+    eax.plot(T, ENERGY, label="full energy")
+    eax.plot(T, KINETIC, label="kinetic energy")
+    eax.plot(T, POTENTIAL, label="potential energy")
+    eax.set_xlabel("t (s)")
+    eax.set_ylabel("E/m J/kg")
+    eax.legend()
 
-eax.legend()
-plt.show()
+    fig.savefig(f"./images/{NAME}-energy.eps")
+
+if "-tbs" in sys.argv:
+    pass
+
+# plt.show()
+
+print("Done")
---
+++ analysers/totex/README.md
@@ -0,0 +1,6 @@
+
+This is my utility lib converting tables to .TEX tables
+
+Partly is stolen from another lib which name I forgot succesfully
+
+To use just import totex
